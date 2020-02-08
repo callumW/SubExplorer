936171daea68e26a39fcaa1acb6ff3842d0c617b
Callum Wilson <calwilson@live.co.uk>
2020-02-08 19:13:47 +0000

    Added auto-level behaviour to sub

---
+++ Assets/Scripts/AutoLevelBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoLevelBehaviour : MonoBehaviour
+{
+    public float smoothTime = 1f;
+    private Vector3 velocity;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 curRot = transform.eulerAngles;
+        Vector3 toRot = new Vector3(0f, curRot.y, 0f);
+
+        transform.eulerAngles = toRot;
+    }
+}
